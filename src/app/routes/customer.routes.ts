f84c773550415d43dd29901a8d7af163642110d6
CodeEver-CEO <raviraj20517@gmail.com>
2026-09-13 15:50:11 +0530

    feat: add admin customer management

--- src/app/routes/customer.routes.ts
+++ src/app/routes/customer.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 
 import {
+  APP_ROLES
+} from '../core/constants/app-roles';
+import {
   authGuard
 } from '../core/guards/auth.guard';
+import {
+  roleGuard
+} from '../core/guards/role.guard';
 
 export const customerRoutes: Routes = [
   {
@@ -12,8 +18,29 @@ export const customerRoutes: Routes = [
       import(
         '../features/customer/pages/profile/profile.component'
       ).then(
-        component => component.ProfileComponent
+        component =>
+          component.ProfileComponent
       ),
     title: 'My Profile | Event Parking'
+  },
+  {
+    path: 'admin/customers',
+    canActivate: [
+      authGuard,
+      roleGuard
+    ],
+    data: {
+      roles: [
+        APP_ROLES.admin
+      ]
+    },
+    loadComponent: () =>
+      import(
+        '../features/customer/pages/admin-customer-management/admin-customer-management.component'
+      ).then(
+        component =>
+          component.AdminCustomerManagementComponent
+      ),
+    title: 'Manage Customers | Event Parking'
   }
 ];
